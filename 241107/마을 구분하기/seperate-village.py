f0f4493e1fb1ba198e37a6e860d83f50e21b8d66
Chae-eun-Lee <chenny2001@gmail.com>
2024-11-07 16:39:48 +0900

    Title: 마을 구분하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241107/마을 구분하기/seperate-village.py
@@ -0,0 +1,36 @@
+def in_range(x, y, n):
+    return 0 <= x < n and 0 <= y < n
+
+def dfs(x, y, grid, visited):
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    stack = [(x, y)]
+    visited[x][y] = True
+    s = 1
+    while stack:
+        cx, cy = stack.pop()
+        for dx, dy in directions:
+            nx, ny = cx + dx, cy + dy
+        
+            if in_range(nx, ny, len(grid)) and not visited[nx][ny] and grid[nx][ny] == 1:
+                visited[nx][ny] = True
+                stack.append((nx, ny))
+                s += 1
+    return size
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+count = 0
+size = []
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1 and not visited[i][j]:
+            s = dfs(i, j, grid, visited)
+            size.append(s)
+            count += 1
+
+print(count)
+for s in sorted(size):
+    print(s)
